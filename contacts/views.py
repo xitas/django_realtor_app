f173cdbdf753c0c8b267e41ede11f6259c115f05
xitas <xitas.ameer9@gmail.com>
2023-01-20 17:19:09 +0500

    first commit

---
+++ contacts/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.core.mail import send_mail
+from .models import Contact
+
+def contact(request):
+    if request.method == 'POST':
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
+
+        # check if user have made inquiry
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, 'You already have made an inquery for this listing')
+                return redirect('/listings/' + listing_id)
+
+        contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
+
+        contact.save()
+
+        # send email
+        send_mail(
+            'Property Listing Inquery',
+            'There has been an Inquery ' + listing + '. Sign into admin panel for more.',
+            'from@gmail.com',
+            [realtor_email, 'to@gmail.com'],
+            fail_silently=False
+        )
+
+        messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
+
+        return redirect('/listings/' + listing_id)
